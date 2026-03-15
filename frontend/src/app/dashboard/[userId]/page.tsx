0162bd6148d9bbcab5dbe9c88a1995a3d373f8da
jiaman <e1138914@u.nus.edu>
2026-03-15 14:49:07 +0800

    Update page.tsx

--- frontend/src/app/dashboard/[userId]/page.tsx
+++ frontend/src/app/dashboard/[userId]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState, useRef } from "react";
 import Image from "next/image";
 import { useParams, useRouter, useSearchParams } from "next/navigation";
 import {
